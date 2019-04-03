e217bcb13f77f47d61edd0f7378da04529be8a80
ontofractal <ontofractal@protonmail.com>
2019-04-03 17:22:18 +0100

    add response mapper middleware

---
+++ lib/exnomics/middleware/response_mapper.ex
@@ -0,0 +1,35 @@
+defmodule Exnomics.ResponseMapper do
+  @behaviour Tesla.Middleware
+  alias Exnomics.{Market}
+
+  @moduledoc """
+  Parses and maps API responses to corresponding structs using idiomatic underscored atom keys, etc.
+  """
+
+  def call(env, next, _options) do
+    env
+    |> Tesla.run(next)
+    |> handle_response()
+  end
+
+  def handle_response(response) do
+    with {:ok, env} <- response do
+      url = env.url
+
+      builder =
+        cond do
+          url =~ ~r|/markets$| ->
+            &struct!(Market, &1)
+
+          true ->
+            {:error, "Response mapper middleware has no builder for this request url: #{env.url}"}
+        end
+
+      builder_mapper = &(MapKeys.to_atoms_unsafe!(&1) |> Enum.map(builder))
+      env = update_in(env.body, builder_mapper)
+      {:ok, env}
+    else
+      err -> err
+    end
+  end
+end
