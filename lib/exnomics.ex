77d43d4cba6bfaef96fe031e75f41ff35ac6de4f
ontofractal <ontofractal@protonmail.com>
2019-04-03 15:34:40 +0100

    init new project ( ﾉ ﾟｰﾟ)ﾉ☀️

---
+++ lib/exnomics.ex
@@ -0,0 +1,18 @@
+defmodule Exnomics do
+  @moduledoc """
+  Documentation for Exnomics.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Exnomics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/exnomics_test.exs
@@ -0,0 +1,8 @@
+defmodule ExnomicsTest do
+  use ExUnit.Case
+  doctest Exnomics
+
+  test "greets the world" do
+    assert Exnomics.hello() == :world
+  end
+end
