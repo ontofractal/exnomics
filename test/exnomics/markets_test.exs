e27b2d3526590f21b77e9673192b001455653b44
ontofractal <ontofractal@protonmail.com>
2019-04-03 17:30:44 +0100

    add test for markets with opts

--- test/exnomics/markets_test.exs
+++ test/exnomics/markets_test.exs
@@ -8,4 +8,13 @@ defmodule Exnomics.MarketsTest do
   test "get_markets" do
     assert {:ok, %{body: [%Market{} | _]}} = get_markets(@client)
   end
+
+  test "get_markets with opts" do
+    assert {:ok, %{body: [%Market{} | _]}} =
+             get_markets(@client,
+               exchange: "binance",
+               base: "BTC,ETH,LTC,XMR",
+               quote: "BTC,ETH,BNB"
+             )
+  end
 end
