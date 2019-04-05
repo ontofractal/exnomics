9eb69816c42b15d9c535967701c37514761f3982
ontofractal <ontofractal@protonmail.com>
2019-04-05 18:49:19 +0100

    improve ohlcv testing

--- test/exnomics/ohlcv_test.exs
+++ test/exnomics/ohlcv_test.exs
@@ -6,8 +6,22 @@ defmodule Exnomics.OhlcvTest do
   @client Exnomics.new()
 
   test "get ohlcvs/candles" do
-    assert {:ok, %{body: [%Ohlcv{} | _]}} =
+    assert {:ok, %{body: ohlcvs = [%Ohlcv{} | _]}} =
              get_exchange_candles(
+               @client,
+               "1d",
+               "binance",
+               "BTCUSDT",
+               start: "2018-03-14T00-00-00Z",
+               end: "2018-04-14T00-00-00Z"
+             )
+
+    assert length(ohlcvs) == 31
+  end
+
+  test "defdelegate get ohlcvs/candles" do
+    assert {:ok, %{body: [%Ohlcv{} | _]}} =
+             Exnomics.get_ohlcvs(
                @client,
                "1d",
                "binance",
