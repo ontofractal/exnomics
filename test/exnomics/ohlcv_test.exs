0325cb2e99440a7977a6a100d4dddd8cda27a9fe
ontofractal <ontofractal@protonmail.com>
2019-04-08 12:44:28 +0100

    add test for aggregated candles

--- test/exnomics/ohlcv_test.exs
+++ test/exnomics/ohlcv_test.exs
@@ -27,6 +27,26 @@ defmodule Exnomics.OhlcvTest do
     assert length(ohlcvs) == 31
   end
 
+  test "get aggregated ohlcvs/candles" do
+    assert {:ok, %{body: ohlcvs = [%Ohlcv{} | _]}} =
+             get_aggregated_candles(
+               @client,
+               "1d",
+               "BTC",
+               start: ~N[2018-04-14 00:00:00],
+               end: ~N[2018-05-14 00:00:00]
+             )
+
+    assert {:ok, %{body: _ = [%Ohlcv{} | _]}} =
+             get_aggregated_candles(
+               @client,
+               "1d",
+               "BTC"
+             )
+
+    assert length(ohlcvs) == 31
+  end
+
   test "defdelegate get ohlcvs/candles" do
     assert {:ok, %{body: [%Ohlcv{} | _]}} =
              Exnomics.get_ohlcvs(
