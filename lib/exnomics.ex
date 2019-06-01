75752ced980c5ff89515ae2665d18ea3ded4f60e
ontofractal <ontofractal@protonmail.com>
2019-06-01 21:51:50 +0300

    fix delegation functions

--- lib/exnomics.ex
+++ lib/exnomics.ex
@@ -2,17 +2,28 @@ defmodule Exnomics do
   @moduledoc """
   Exnomics API client built using Tesla
   """
-  defdelegate get_markets(client, opts), to: Exnomics.Markets
+  alias Exnomics.{Ohlcvs, Markets, Currencies}
 
-  defdelegate get_exchange_candles(client, interval, exchange, market, opts), to: Exnomics.Ohlcvs
+  defdelegate get_markets(client, opts), to: Markets
+  defdelegate get_markets(client), to: Markets
+
+  defdelegate get_exchange_candles(client, interval, exchange, market, opts), to: Ohlcvs
 
   defdelegate get_ohlcvs(client, interval, exchange, market, opts),
-    to: Exnomics.Ohlcvs,
+    to: Ohlcvs,
     as: :get_exchange_candles
 
   defdelegate get_ohlcvs(client, interval, exchange, market),
-    to: Exnomics.Ohlcvs,
+    to: Ohlcvs,
     as: :get_exchange_candles
+
+  defdelegate get_aggregated_ohlcvs(client, interval, currency, opts),
+    to: Ohlcvs,
+    as: :get_aggregated_candles
+
+  defdelegate get_aggregated_ohlcvs(client, interval, currency),
+    to: Ohlcvs,
+    as: :get_aggregated_candles
 
   @base_url "https://api.nomics.com/v1"
 
